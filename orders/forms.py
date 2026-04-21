b9fc1b080bd31dde2a4aac749fe44da7b74f03fd
Richard Mweresa <munric2697@gmail.com>
2026-04-21 18:08:55 +0300

    fixed customer_name migration

--- orders/forms.py
+++ orders/forms.py
@@ -5,5 +5,3 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['customer_name', 'phone', 'details']
-
-        fields = ['customer_name', 'phone', 'details', 'mpesa_code']
